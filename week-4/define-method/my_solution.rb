6b250a810bafdb2837fa8e3a29b8512a2c9134c8
Kyle Rombach <kylerombach@Kyles-iMac.local>
2016-01-25 11:16:26 -0800

    Edit defining variable

--- week-4/define-method/my_solution.rb
+++ week-4/define-method/my_solution.rb
@@ -6,3 +6,31 @@
 # Your Solution Below
 def do_something(city, state, zip)
 end
+
+=begin
+
+How do you define a local variable?
+
+You just make whatever variable you want to use and set it equal to what you want it to be. So you could do number = 1, as an example.
+
+How do you define a method?
+
+You have to put def before the method you want to define. An example would be, def movie_ratings.
+
+What is the difference between a local variable and a method?
+
+With a variable you're just assigning it a certain value, while when you use a method, it will actually perfrom a task.
+
+How do you run a ruby program from the command line?
+
+You can either open up IRB and type your ruby code in there or make a .rb file and do ruby file-name.rb as well.
+
+How do you run an RSpec file from the command line?
+
+You just write rspec file-name.rb and it will run you a test.
+
+What was confusing about this material? What made sense?
+
+Just getting the syntax right for the address but the numbers one was super easy.
+
+=end
