3975bb9bd42eb0dcbb395f85a691858107df3d01
Kyle Rombach <kylerombach@Kyles-iMac.local>
2016-02-01 08:50:31 -0800

    Edd numbers with commas

--- week-5/nums-commas/my_solution.rb
+++ week-5/nums-commas/my_solution.rb
@@ -37,3 +37,26 @@ def separate_comma(number)
 end
 
 # 3. Reflection
+=begin
+
+- What was your process for breaking the problem down? What different approaches did you consider?
+
+I wanted to think of the best way to split up the number to add the comma. I figure splitting it into groups of three and then adding in the comma inbetween the groups would be best. I thought about even inserting a comma directly into the array but I felt I wouldn't be able to get a consist result that way.
+
+- Was your pseudocode effective in helping you build a successful initial solution?
+
+Yea it made me realize how I need to keep breaking down everything into seperate arrays and then join them back together. I had to make sure the proccess would repeat for the whole number.
+
+- What new Ruby method(s) did you use when refactoring your solution? Describe your experience of using the Ruby documentation to implement it/them (any difficulties, etc.). Did it/they significantly change the way your code works? If so, how?
+
+I used slice for both solutions and that's what allowed me to get my numbers into my groups of three. My code for both was pretty much the same, just polishing it up.
+
+- How did you initially iterate through the data structure?
+
+I did the map! method to create the new array of the split numbers and using do for each arrays so the process would continue for the whole array.
+
+- Do you feel your refactored solution is more readable than your initial solution?
+
+Yea because I just made it more of a streamlined proccess.
+
+=end
